174291c452ceb9a2b8bc83f5c15c93effe8966be
sbfse <71165328+sbfse@users.noreply.github.com>
2022-01-09 13:26:22 -0600

    fixing html file.

---
+++ bookmarks.js
@@ -0,0 +1,89 @@
+// Note: having length != 4 will mess with layout based on how the site is styled
+const bookmarks = [
+  {
+    title: "social.",
+    links: [
+      { name: "Reddit.", url: "https://www.reddit.com" },
+      { name: "Facebook.", url: "https://www.facebook.com" },
+      { name: "Twitter.", url: "https://www.twitter.com" },
+      { name: "Tumblr.", url: "https://www.tumblr.com" },
+      { name: "Discord.", url: "https://discord.com/channels/@me" },
+      { name: "Revolt.", url: "https://app.revolt.chat/" },
+      { name: "Spacehey.", url: "https://www.spacehey.com" },
+    ],
+  },
+  {
+    title: "media.",
+    links: [
+      { name: "Youtube.", url: "https://youtube.com" },
+      { name: "Netflix.", url: "https://netflix.com" },
+      { name: "Spotify.", url: "https://open.spotify.com/" },
+      { name: "Deezer.", url: "https://www.deezer.com/us/profile/1972525046/playlists" },
+      { name: "Radio.G.", url: "http://radio.garden" },
+      { name: "Plaza.", url: "http://plaza.one" },
+      { name: "Lofi.", url: "https://lofi.cafe" },
+      { name: "Pool.", url: "https://poolsuite.net/" },
+      { name: "NR.fm.", url: "https://nightride.fm/" },
+      { name: "Twitch.", url: "https://twitch.tv" },
+      { name: "Last.FM.", url: "https://www.last.fm/user/sbfse" },
+    ],
+  },
+  {
+    title: "general.",
+    links: [
+      { name: "4chan.", url: "https://www.4chan.org" },
+      { name: "Mobilism.", url: "https://forum.mobilism.org/viewforum.php?f=399" },
+      { name: "XDA.", url: "https://forum.xda-developers.com/" },
+      { name: "deviantART.", url: "https://www.deviantart.com" },
+      { name: "RetroACHV.", url: "https://retroachievements.org/user/subfuse" },
+      { name: "s-ul.", url: "https://s-ul.eu/files" },
+      { name: "GetComics.", url: "https://getcomics.info/" },
+      { name: "Stumbled.", url: "https://stumbled.to/stumble" },
+      { name: "StumblingOn.", url: "https://stumblingon.com/" },
+      { name: "1337x.to.", url: "https://1337x.to/" },
+      { name: "ayes.tk.", url: "https://ayes.tk/search/" },
+      { name: "/r/FMHY.", url: "https://www.reddit.com/r/FREEMEDIAHECKYEAH/wiki/index" },
+    ],
+  },
+  {
+    title: "coding.",
+    links: [
+      { name: "Freecode.", url: "https://www.freecodecamp.org/learn" },
+      { name: "CodeCDMY.", url: "https://www.codecademy.com/profiles/sbfse" },
+      { name: "Exercism.", url: "https://exercism.org/dashboard" },
+      { name: "edX.", url: "https://courses.edx.org/dashboard" },
+      { name: "Vercel.", url: "https://vercel.com/dashboard" },
+      { name: "Code.org.", url: "https://studio.code.org/home" },
+      { name: "w3school.", url: "https://my-learning.w3schools.com/" },
+      { name: "Coursera.", url: "https://www.coursera.org/search?query=solidity&" },
+      { name: "Stack.", url: "https://stackoverflow.com/" },
+      { name: "Hacksplain.", url: "https://www.hacksplaining.com/lessons" },
+    ],
+  },
+ {
+    title: "html.",
+    links: [
+      { name: "Bearblog.", url: "https://bearblog.dev/dashboard/" },
+      { name: "Geocities.", url: "https://members.geocities.ws/fm/index.php" },
+      { name: "Neocities.", url: "https://neocities.org/dashboard" },
+    ],
+  },
+ {
+    title: "crypto.",
+    links: [
+      { name: "OpenSea.", url: "https://opensea.io/account" },
+      { name: "Coingecko.", url: "https://www.coingecko.com/en" },
+      { name: "Portis.", url: "https://wallet.portis.io/" },
+      { name: "Blockchain.", url: "https://login.blockchain.com/en/#/home" },
+    ],
+  },
+  {
+    title: "emails.",
+    links: [
+      { name: "Gmail.", url: "https://www.gmail.com" },
+      { name: "Live.", url: "https://www.live.com" },
+      { name: "Proton.", url: "https://mail.protonmail.com/u/0/inbox" },
+      { name: "Fake.", url: "https://www.fakemailgenerator.com" },
+    ],
+  },
+];
